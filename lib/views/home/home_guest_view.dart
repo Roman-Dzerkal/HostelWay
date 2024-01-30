9ea77f2e4349b8736825314da0eb524c9d013d3b
mmyr <r.dzerkal@pinta.pro>
2024-01-30 23:50:03 +0200

    Deleted unnecessary imports

--- lib/views/home/home_guest_view.dart
+++ lib/views/home/home_guest_view.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hostelway/repositories/hotels_repository.dart';
 import 'package:hostelway/views/home/home_guest/bloc/home_guest_bloc.dart';
 import 'package:hostelway/views/home/navigation/home_guest_navigator.dart';
-import 'package:hostelway/widget_helpers/custom_hotel_item.dart';
 import 'package:hostelway/widget_helpers/custom_navbar/guest_navbar.dart';
 import 'package:hostelway/widget_helpers/custom_navbar/navigation/guest_navigator.dart';
 
