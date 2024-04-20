186705f2bd5d6e33ffb93b4ecded1ebaff9fddec
mmyr <r.dzerkal@pinta.pro>
2024-04-20 13:27:33 +0300

    Fixed loading project

--- lib/main.dart
+++ lib/main.dart
@@ -34,14 +34,6 @@ Future<void> main() async {
   const supabaseAnonKey =
       String.fromEnvironment('SUPABASE_ANON_KEY', defaultValue: 'aboba3');
 
-  String userId = 'd88bfb74-b04f-4ffb-a0aa-851db86c0e6c';
-  List<String>? abobaList = await DataClass.getUserMetadata<List<String>?>('aboba_list', userId);
-  if (abobaList != null) {
-    debugPrint(abobaList.toString());
-  } else {
-    throw Exception('abobaList is null');
-  }
-
   await Supabase.initialize(
     url: supabaseUrl,
     anonKey: supabaseAnonKey,
