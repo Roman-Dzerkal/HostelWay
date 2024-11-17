949e4f54a2711e7607838573b2fa4e02cd382431
aprilliss <aprilliss.2001@gmail.com>
2024-11-17 15:33:55 +0200

    sign in fixes

--- lib/presentation/views/auth/sign_in/bloc/sign_in_bloc.dart
+++ lib/presentation/views/auth/sign_in/bloc/sign_in_bloc.dart
@@ -1,11 +1,14 @@
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:hostelway/app/auth_bloc/authentication_bloc.dart';
+import 'package:hostelway/domain/models/user_model.dart';
 import 'package:hostelway/presentation/views/auth/sign_in/models/sign_in_error_state.dart';
 import 'package:hostelway/presentation/views/auth/sign_in/navigation/sign_in_navigator.dart';
 import 'package:hostelway/data/data_sources/validation_service.dart';
+import 'package:hostelway/utils/tost_util.dart';
 
 part 'sign_in_event.dart';
 part 'sign_in_state.dart';
@@ -29,10 +32,53 @@ class SignInBloc extends Bloc<SignInEvent, SignInState> {
     });
 
     on<SignInSubmitted>((event, emit) async {
-      final UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: state.email,
-        password: state.password,
-      );
+      String errorMessage = '';
+      UserCredential user;
+      if (validForm(emit) == false) {
+        return;
+      }
+      emit(state.copyWith(isBusy: true));
+      try {
+        user = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: state.email,
+          password: state.password,
+        );
+      } on FirebaseAuthException catch (e) {
+        switch (e.code) {
+          case 'user-not-found':
+            errorMessage = 'User with this email not found.';
+            break;
+          case 'wrong-password':
+            errorMessage = 'Wrong password';
+            break;
+          case 'invalid-credential':
+            errorMessage =
+                'Invalid credentials. Please check your input or try again.';
+            break;
+          default:
+            errorMessage = 'System error: ${e.code} - ${e.message}';
+            break;
+        }
+        if (errorMessage.isNotEmpty) {
+          ToastUtil.showError(errorMessage);
+        }
+        return;
+      }
+      try {
+        final doc = await FirebaseFirestore.instance
+            .collection('User')
+            .doc(user.user!.uid)
+            .get();
+
+        UserModel userModel = UserModel.fromJson(doc.data()!);
+        emit(state.copyWith(isBusy: false));
+        authenticationBloc.add(AuthenticationSaveUserInformationEvent(
+            userModel: userModel, voidCallback: navigator.goToHomePage));
+        //ToastUtil.showError( '${doc.data()?['first_name']}');
+      } on Exception catch (e) {
+        //print("Error getting document: $e");
+        ToastUtil.showError("$e");
+      }
     });
 
     on<SignInWithGooglePressed>((event, emit) async {
